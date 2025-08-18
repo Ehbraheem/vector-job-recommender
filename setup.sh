c30a0da52c075e20c0801d6261621204208288da
Bolatan Ibrahim <ehbraheem@gmail.com>
2025-08-18 19:17:59 -0400

    Add command to download data

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ npm i
 
 # Download seed data
 wget -O ./data/jobPostings.cjs https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/PuG2Nqinq_7G71t1NPOJeQ/jobPostings.js
+wget -P ./data https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/4V1kcXr8NDjTN6Sof83-4w/testResume.pdf
