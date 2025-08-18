8ba8c926d64758d7c47275ff7604203e8c81a401
Bolatan Ibrahim <ehbraheem@gmail.com>
2025-08-18 18:49:20 -0400

    Setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install dependecies
+npm i
+
+# Download seed data
+wget -O ./data/jobPostings.cjs https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/PuG2Nqinq_7G71t1NPOJeQ/jobPostings.js
